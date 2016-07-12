b2fd345a8e12599945e35e6358c143c9f6c4fa1b
August Shi <awshi2@illinois.edu>
2016-07-12 15:50:23 -0500

    Initial

---
+++ src/main/java/simple/testing/App.java
@@ -0,0 +1,14 @@
+package simple.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public int foo()
+    {
+        int x = 0;
+        return x;
+    }
+}
---
+++ src/test/java/simple/testing/AppTest.java
@@ -0,0 +1,23 @@
+package simple.testing;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testFoo1()
+    {
+        App a = new App();
+        a.foo();
+    }
+
+    @Test
+    public void testFoo2()
+    {
+        App a = new App();
+        a.foo();
+    }
+}
